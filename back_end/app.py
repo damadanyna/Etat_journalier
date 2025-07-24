e4b253d1ea6cd25ddd0b7fbb93acb30948fe6345
DAMA DANY <damadanyna@gmail.com>
2025-07-24 08:36:09 +0300

    Correction de l'importation des fichier

--- back_end/app.py
+++ back_end/app.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.gzip import GZipMiddleware  # ✅ Compression gzip
-
 from controller.Credits import Credits
-from api.api import api_router
+from api.api import api_router   
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# ✅ Middleware GZip pour compresser les grandes réponses JSON (ex: 5 Mo)
-app.add_middleware(GZipMiddleware, minimum_size=1000)
 
-# ✅ Middleware CORS pour accepter les requêtes cross-origin
+# Middleware CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -19,11 +15,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ✅ Initialiser la classe Credits
+ 
+# Initialiser la classe Credits
 credits = Credits()
 
-# ✅ Enregistrer les routes de l'API
+# Enregistrer les routes de l'API
 app.include_router(api_router, prefix="/api")
 
-# ✅ Pour lancer :
-# uvicorn app:app --reload --host 0.0.0.0 --port 8000
+# Pour lancer : uvicorn app:app --reload --port 8081
