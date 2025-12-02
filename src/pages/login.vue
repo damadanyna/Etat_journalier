fe77e9f1cd74dc31301c81384042f59ed20aa193
Dama Dany <damadanyna@gmail.com>
2025-12-02 16:22:38 +0300

    On branch log_paie Changes to be committed: 	modified:   src/pages/login.vue

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -8,6 +8,10 @@
             Tout  en <br />
             quelques minutes seulement. <strong>Baiboho</strong>
         </p>
+        <div class="flex">
+            acceder à <span @click="change_page()" class=" ml-1 underline text-green-800 font-extrabold cursor-pointer">Pay By</span>
+      
+        </div>
       </div>
 
       <!-- Right side login form -->
